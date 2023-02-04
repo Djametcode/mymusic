9d2e5db522c440ceb98f72a65fe7814281815098
djametcode <kampretsiji1@gmail.com>
2023-02-05 03:05:19 +0700

    changes

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -19,7 +19,7 @@ const List = ({data}) => {
 
     const thumbnail = `https://api.napster.com/imageserver/v2/albums/${albumId}/images/356x237.jpg`;
     return (
-        <div className=" m-2 flex flex-col justify-center text-center gap-2 p-3 bg-white/30 rounded-3xl cursor-pointer">
+        <div className=" m-2 flex flex-col justify-center text-center gap-2 p-3 bg-white/30 rounded-3xl cursor-pointer snap-center">
                 <img className=" rounded-3xl" src={thumbnail} alt="" />
                 <h1 className=" w-64">{name}</h1>
                 <h2>{artistName}</h2>
@@ -47,7 +47,7 @@ const MusicList = () => {
     }, [])
     const result = data.map((item) => <List key={item.id} data = {item} />)
     return(
-        <div className=" flex overflow-auto p-3">
+        <div className=" flex overflow-auto p-3 snap-x snap-mandatory">
             {result}
         </div>
     )
